307f25a7c2c6559d8270ee0157e1b8a38fecc9ee
Amine42 <amine.bouraghda@gmail.com>
2019-01-23 00:48:41 +0100

    PERFECT

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 require 'bundler'
 Bundler.require
 
-# Récupère la class ScapTownhall
+# Récupère toutes les classes pour pouvoir les appeler plus simplement
 
 $:.unshift File.expand_path("./../lib/app", __FILE__)
 require 'scrapper'
@@ -12,6 +12,18 @@ require 'scrapper'
 
 email = ScrapTownhall.new
 
-# Affiche tous les ville et les emails qui y corresponde
+# stock tout les nom et email dans une variable de class
 
 email.get_name_and_email_townhall
+
+# sauvegarde les nom de ville et leur email dans un csv
+
+email.save_as_csv
+
+# sauvegarde les nom de ville et leur email dans un json
+
+email.save_as_JSON
+
+# sauvegarde les nom de ville et leur email dans le spreadsheet
+
+email.save_as_spreadsheet
